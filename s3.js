717ee1866860b8cb9b9506597a8f77ad60757ba0
lee <o5046821854@gmail.com>
2021-08-22 23:44:16 +0900

    first commit

--- s3.js
+++ s3.js
@@ -1,6 +1,40 @@
 require('dotenv').config()
-const s3 = require('aws-sdk')
-const bucketNAme = process.env.AWS_BUCKET_NAME="lee-exercise"
-const region = process.env.AWS_BUCKET_REGION=" ap-northeast-2"
-const accessKey = process.env.AWS_ACCESS_KEY="AKIA2KZALC67MKBA57G2"
-const secretKey = process.env.AWS_SECRET_KEY="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"
+const fs = require('fs')
+const S3 = require('aws-sdk/clients/s3')
+
+const bucketName = process.env.AWS_BUCKET_NAME
+const region = process.env.AWS_BUCKET_REGION
+const accessKeyId = process.env.AWS_ACCESS_KEY
+const secretAccessKey = process.env.AWS_SECRET_KEY
+
+const s3 = new S3({
+  region,
+  accessKeyId,
+  secretAccessKey
+})
+
+// uploads a file to s3
+function uploadFile(file) {
+  const fileStream = fs.createReadStream(file.path)
+
+  const uploadParams = {
+    Bucket: bucketName,
+    Body: fileStream,
+    Key: file.filename
+  }
+
+  return s3.upload(uploadParams).promise()
+}
+exports.uploadFile = uploadFile
+
+
+// downloads a file from s3
+function getFileStream(fileKey) {
+  const downloadParams = {
+    Key: fileKey,
+    Bucket: bucketName
+  }
+
+  return s3.getObject(downloadParams).createReadStream()
+}
+exports.getFileStream = getFileStream
--- sql.js
+++ sql.js
@@ -1,12 +1,27 @@
 const mysql = require('mysql')
 const express=  require('express')
 const fs = require('fs')
-
+const multer = require('multer')
 const app = express()
+const {uploadFile, getFileStream } = require('./s3.js')
+const multerS3 = require('multer-s3');
 const data = fs.readFileSync('./database.json')
 const conf = JSON.parse(data)
+const bodyparser = require('body-parser')
 
 const PORT = 3000 || process.env.PORT
+app.use(bodyparser.json())
+app.use(bodyparser.urlencoded({extended: true}))
+
+app.set('view engine', 'ejs'); 
+app.engine('html', require('ejs').renderFile);
+
+const aws = require('aws-sdk');
+const s3 = new aws.S3()
+const upload = multer({
+    dest: "logo"
+});
+
 
 const db = mysql.createConnection({
     host: conf.host,
@@ -28,6 +43,26 @@ app.get('', (req,res)=>{
     })
 })
 
+
+app.get('/create', upload.single('logo'), (req,res)=>{
+    res.render('create.html')
+})
+
+app.post('/add',upload.single("logo"), async(req, res)=>{
+    console.log(req.body)
+    console.log(req.file)
+    console.log(req.body.name)
+    let sql = 
+    `INSERT INTO POST (name, category, campus, author, logo ) VALUES ("${req.body.name}", "${req.body.category}", "${req.body.campus}", "${req.body.author}", "${req.file.originalname}")`
+    const result = await uploadFile(req.file)
+    db.query(sql, (err, result)=>{
+        if(err)
+            throw err;
+        console.log(result)
+        res.redirect('/')
+    })
+})
+
 app.get('/add', (req, res)=>{
     let sql = 
     'INSERT INTO POST (name, category, campus, author, logo ) VALUES ("멋사2", "코딩", "명륜", "이영신", "logo.png")'
@@ -46,7 +81,7 @@ app.get('/update', (req, res)=>{
         if(err)
             throw err;
         console.log(result)
-        res.send('database updated')
+        res.redirect('/')
     })
 })
 
@@ -57,7 +92,7 @@ app.get('/delete', (req, res)=>{
         if(err)
             throw err;
         console.log(result)
-        res.send('database deleted')
+        res.render('index.html')
     })
 })
 
